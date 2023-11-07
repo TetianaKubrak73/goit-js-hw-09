c433e2bb0e758cf709a49a4b58a86ad9c6a92309
TetianaKubrak73 <tetianakubrak73@gmail.com>
2023-11-07 22:13:17 +0100

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+function getRandomHexColor() {
+   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
+
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+const body = document.body;
+let intervalId;
+
+function changeBackgroundColor() {
+    body.style.backgroundColor = getRandomHexColor();
+}
+
+startButton.addEventListener('click', () => {
+        if (!intervalId) {
+        intervalId = setInterval(changeBackgroundColor, 1000); 
+        startButton.disabled = true;
+        stopButton.disabled = false;
+    }
+});
+
+stopButton.addEventListener('click', () => {
+        if (intervalId) {
+        clearInterval(intervalId); 
+        intervalId = null;
+        startButton.disabled = false;
+        stopButton.disabled = true;
+    }
+});
 
